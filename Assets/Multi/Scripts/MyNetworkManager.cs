cdd5313e5f4f522d86140b688fd224a229c37c51
Marcin-PC\Marcin <marcin.lazowski@perfectworld.biz.pl>
2016-08-17 10:45:19 +0200

    Unet Work-in-progress

---
+++ Assets/Multi/Scripts/MyNetworkManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using UnityEngine.Networking.Match;
+
+public class MyNetworkManager : NetworkManager {
+
+	public static MyNetworkManager mgr;
+
+	void Awake()
+	{
+		if (mgr == null)
+			mgr = this;
+		else if (mgr != this)
+			Destroy (gameObject);
+	}
+
+	public override void OnMatchCreate (bool success, string extendedInfo, UnityEngine.Networking.Match.MatchInfo matchInfo)
+	{
+		base.OnMatchCreate (success, extendedInfo, matchInfo);
+		Debug.Log ("Match created");
+	}
+    public override void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+    {
+        base.OnMatchList(success, extendedInfo, matchList);
+
+        if (!success)
+            Debug.Log("MatchList failed");
+        else
+        {
+            Debug.Log("MatchList passed");
+            NetworkController.cont.listMatches = true;
+        }
+    }
+    public override void OnMatchJoined(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        base.OnMatchJoined(success, extendedInfo, matchInfo);
+    }
+}
---
+++ Assets/Multi/Scripts/NetworkController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.Networking.Types;
+
+public class NetworkController : MonoBehaviour {
+
+    public static NetworkController cont;
+    NetworkManager mgr;
+	NetworkMatch networkMatch;
+    public bool listMatches = false;
+
+	void Start()
+	{
+        cont = this;
+        mgr = MyNetworkManager.mgr;
+		StartMatchmaking ();
+	}
+	void StartMatchmaking ()
+	{
+		mgr.StartMatchMaker ();	
+		networkMatch = gameObject.AddComponent<NetworkMatch> ();
+	}
+
+
+    void OnGUI()
+	{
+        int spacing = 24;
+        int ypos = 40;
+        if (!listMatches)
+        {
+            if (GUILayout.Button("Create match"))
+            {
+                networkMatch.CreateMatch("new room", 4, true, "", "", "", 0, 1, mgr.OnMatchCreate);
+            }
+            if (GUILayout.Button("List matches"))
+            {
+                networkMatch.ListMatches(0, 10, "", false, 0, 1, mgr.OnMatchList);
+            }
+        }
+        else
+        {
+            Debug.Log(mgr.matches.Count + " is the number of matches");
+            foreach (var match in mgr.matches)
+            {
+                if (GUILayout.Button("Join Match:" + match.name))
+                {
+                    mgr.matchName = match.name;
+                    mgr.matchSize = (uint)match.currentSize;
+                    mgr.matchMaker.JoinMatch(match.networkId, "", "", "", 0, 1, mgr.OnMatchJoined);
+                }
+                Debug.Log(match.name);
+                ypos += spacing;
+            }
+        }
+    }
+}
